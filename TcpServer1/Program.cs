707cf1c643bc8db84a1bb6dfb5f9ce6cd1abcb7c
Arash <mh.norouzi89@gmail.com>
2020-05-31 16:10:19 +0430

    first

---
+++ TcpServer1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TcpServer1
+{
+    class Program
+    {
+        private const int portNum = 13;
+
+        public static int Main(String[] args)
+        {
+            bool done = false;
+
+            var listener = new TcpListener(IPAddress.Any, portNum);
+
+            listener.Start();
+
+            while (!done)
+            {
+                Console.Write("Waiting for connection...");
+                TcpClient client = listener.AcceptTcpClient();
+
+                Console.WriteLine("Connection accepted.");
+                NetworkStream ns = client.GetStream();
+
+                byte[] byteTime = Encoding.ASCII.GetBytes(DateTime.Now.ToString());
+
+                try
+                {
+                    ns.Write(byteTime, 0, byteTime.Length);
+                    ns.Close();
+                    client.Close();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+            }
+
+            listener.Stop();
+
+            return 0;
+        }
+    }
+}
